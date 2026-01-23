48e8469fc6597c1721f20fda993efd5762396e61
MoyeAnthonia <moye.anthonia@gmail.com>
2026-01-23 13:43:44 +0000

    fix PEP3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     args_count = len(sys.argv) - 1
 
     if args_count == 0:
-          print(f"{args_count} argument.")
+        print(f"{args_count} arguments.")
     elif args_count == 1:
         print(f"{args_count} argument:")
         print(f"{args_count}:", sys.argv[1])
