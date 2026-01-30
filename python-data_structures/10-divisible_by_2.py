ca14e790b91605c0f9dbae5edf955845cc3074c6
MoyeAnthonia <moye.anthonia@gmail.com>
2026-01-30 12:54:59 +0000

    create file

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+
+    # function that finds all multiples of 2 in a list.
---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,3 @@
+#!bin/usr/python3
+
+def delete_at(my_list=[], idx=0):
