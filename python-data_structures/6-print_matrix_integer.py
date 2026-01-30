a1d3f2b55e9cbb7cc6b4fbd907c5ab26779653bd
MoyeAnthonia <moye.anthonia@gmail.com>
2026-01-30 04:37:02 +0000

    decimal print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for n in matrix:
         for e in n:
-            print("{0}".format(e), end=" ")
+            print("{:d}".format(e), end=" ")
         print("$")
