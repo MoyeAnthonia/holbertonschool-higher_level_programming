189659b62e0801c23c9e323ed9b8190849d107b4
MoyeAnthonia <moye.anthonia@gmail.com>
2026-01-30 03:05:56 +0000

    used list comprehension

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
-        return None
-
-    my_list.reverse()
-    print("{:d}".format(my_list))
-    return my_list
+    [print("{:d}".format(num)) for num in reversed(my_list)]
