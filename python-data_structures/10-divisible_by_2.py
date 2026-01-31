b8c037df8c5ba4a87f0b099dc3f6fc38ad9f20da
MoyeAnthonia <moye.anthonia@gmail.com>
2026-01-31 12:10:09 +0000

    divisble by 2 or not

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
-
-    # function that finds all multiples of 2 in a list.
+    result = []
+    for num in my_list:
+        result.append(num % 2 == 0)
+    return result
