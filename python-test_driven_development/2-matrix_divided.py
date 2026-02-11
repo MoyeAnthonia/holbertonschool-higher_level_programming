330bbb8d6a73eb7b4269e8d86b1c8c081e04cbd1
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-12 01:26:56 +1100

    final fix before test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,22 +21,35 @@ def matrix_divided(matrix, div):
     TypeError: If div is not an integer or float
 
     """
-    if type(div) is not int and type(div) is not float:
-            raise TypeError("div must be a number")
+    
+    if type(div) not in (int, float):
+        raise TypeError("div must be a number")
     if div == 0:
-            raise ZeroDivisionError('division by zero')
+        raise ZeroDivisionError("division by zero")
 
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+    row_length = None
     new_matrix = []
+
     for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        if row_length is None:
+            row_length = len(row)
+        elif len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
         new_row = []
         for element in row:
-            if type(element) is not int and type(element) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            
-    row_length = len(matrix[0])
-    if any(len(row) != row_length for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-    
-    new_row.append(round(element / div, 2))
-    new_matrix.append(new_row)
+            if type(element) not in (int, float):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats"
+                )
+            new_row.append(round(element / div, 2))
+
+        new_matrix.append(new_row)
+
     return new_matrix
