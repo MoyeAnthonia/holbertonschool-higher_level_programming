3f71f33cb1d131cdb016355b5756da5a4a98afec
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-13 14:15:20 +1100

    private size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""empty class Square that defines a square"""
+"""empty class that defines a square"""
 
 
 class Square:
---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a private square attribute"""
+
+
+class Square:
+    def __init__(self, size):
+        """private square attribute"""
+        self.__size = size
