f680dcc362fd2c3d382940a67d7f67b90e69d481
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-13 23:02:35 +1100

    return sqr of size

---
+++ python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a private square attribute"""
+
+
+class Square:
+    """initial class"""
+    def __init__(self, size=0):
+        """private square attribute"""
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Prints the current area of the square"""
+        return self.__size ** 2
