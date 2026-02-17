90ab4d2a20906df06ac8d37eb0f6b6d549f47fc6
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-18 01:07:58 +1100

    inherits from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""object is an instance of inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """checking if instance are correct"""
+    return isinstance(obj, a_class)
---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""exactly an instance of the specified class"""
+
+
+
+def inherits_from(obj, a_class):
+    """returns true if the object's class matches the search case"""
+    return issubclass(obj.__class__, a_class) and obj.__class__ is not a_class
