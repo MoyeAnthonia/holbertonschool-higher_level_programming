cd8ec26040f756867af7922b5288f63c15d6f1da
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-20 16:37:53 +1100

    duck typing tasks

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+"""Creating an abstract class"""
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        """Empty sound method"""
+        pass
+    @abstractmethod
+    def perimeter(self):
+        """Empty sound method"""
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        """insantitied"""
+        super().__init__()
+        self.radius = radius
+
+    def area(self):
+        """call the parent method"""
+        A = 3.14 * (self.radius ** 2)
+        return A
+
+    def perimeter(self):
+        """call the parent method"""
+        C = 3.14 * (self.radius ** 2)
+        return C
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        """insantitied"""
+        super().__init__()
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """call the parent method"""
+        A = self.width * self.height
+        return A
+
+    def perimeter(self):
+        """call the parent method"""
+        P = 2 * (self.width + self.height)
+        return P
+
+def shape_info(entity):
+    """Duck typing function example"""
+    print("Area:", entity.area())
+    print("Perimeter:", entity.perimeter())
