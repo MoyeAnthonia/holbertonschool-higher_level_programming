7cc60aba3514a0328566f5430ca2382f901dea35
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-24 20:06:59 +1100

    fix

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """json return function"""
     with open(filename, "w") as file:
-        return json.dump(my_obj, file, indent=4)
+        return json.dump(my_obj, file)
