dee14d9df09521b7122069919a0dc98ef8ec8c8b
MoyeAnthonia <moye.anthonia@gmail.com>
2026-02-28 11:55:11 +1100

    set up basic auth

---
+++ restful-api/task_05_basic_security.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
+    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+}
+
+# This runs every time a protected route is accessed
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and check_password_hash(users[username]["password"], password):
+        return users[username]
+    return None
+
+# Required Endpoint
+@app.route("/basic-protected", methods=["GET"])
+@auth.login_required
+def basic_protected():
+    return jsonify({
+        "message": "Basic Auth: Access Granted"
+    })
+
+if __name__ == '__main__':
+    app.run()
