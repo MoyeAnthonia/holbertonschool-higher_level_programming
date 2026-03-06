1486cd9ea9db9503ea4ec248c5550da243ec7b81
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-06 12:28:39 +1100

    fix error

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on all databases and tables
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- Reload the privilege tables to ensure changes take effect
 FLUSH PRIVILEGES;
