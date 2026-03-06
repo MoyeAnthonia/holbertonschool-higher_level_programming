bcc2c78954466e0c15f9742c209803ef84cea50f
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-06 23:12:03 +1100

    FIx comments

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,4 @@
+-- count shows
 SELECT tv_genres.name AS genre,
        COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- lists all cities contained in the database hbtn_0d_usa. 
--- usuing JOiN
 SELECT cities.id, cities.name, states.name
 FROM cities
 JOIN states ON cities.states_id = state.id
