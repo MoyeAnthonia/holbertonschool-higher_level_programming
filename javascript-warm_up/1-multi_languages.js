af52bc26278080f15e7fae10819fbcb841421c8b
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-22 23:05:09 +1100

    fix: remove CRLF and add execute permission

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const oneWord = "C is fun";
-const twoWord = "Python is cool";
-const threeWord = "JavaScript is amazing";
+const oneWord = 'C is fun';
+const twoWord = 'Python is cool';
+const threeWord = 'JavaScript is amazing';
 
 console.log(oneWord);
 console.log(twoWord);
