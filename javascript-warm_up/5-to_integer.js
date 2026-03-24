5df48f19fb730bd8271b6089519b348c3c0484f7
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-25 00:09:52 +1100

    fix

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const firstArg = process.argv[2];
-const result = parseInt(firstArg)
-if (isNaN(result))
-    {
-        console.log('Not a number');
-    }
-    else {
-        console.log(`My number: ${result}`);
-    }
+const result = parseInt(firstArg);
+if (isNaN(result)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${result}`);
+}
