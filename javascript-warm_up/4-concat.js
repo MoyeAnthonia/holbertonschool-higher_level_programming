847c1671a9249ea264486fbe903ba313ba362807
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-24 23:22:26 +1100

    fix: remove CRLF and add execute permission

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-const oneArgs = process.argv.slice(2);
-const twoArgs = process.argv.slice(3);
-const [firstArg] = oneArgs;
-const [secondArg] = twoArgs;
-
-const newString = firstArg.concat(" is ", secondArg);
-
-
-if (newString === undefined) {
-  console.log("No argument");
-} else {
-  console.log(newString);
-}
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+console.log(`${firstArg} is ${secondArg}`);
