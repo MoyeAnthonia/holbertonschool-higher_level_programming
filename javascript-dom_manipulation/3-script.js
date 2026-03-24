c0c2e05d6c02d38276288ff8bef079017a3efeb5
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-25 01:03:33 +1100

    fix: remove CRLF and add execute permission

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+document.getElementById("toggle_header").addEventListener("click", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("red");
+  header.classList.toggle("green");
+});
