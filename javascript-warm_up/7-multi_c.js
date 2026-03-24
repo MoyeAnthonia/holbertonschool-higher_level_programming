488db5758a4860b1a9296da7edbdef8285ca8b91
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-24 23:54:57 +1100

    fix: remove CRLF and add execute permission

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = parseInt(process.argv[2]);
+if (isNaN(args)){
+    console.log('Missing number of occurrences');
+}
+else {
+    for (let i = 0; i < args.length; i++){
+    console.log('C is fun');
+}
+}
