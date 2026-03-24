5a25f22321b4b1dc9894459089062e18d8908461
MoyeAnthonia <moye.anthonia@gmail.com>
2026-03-25 00:25:31 +1100

    fix: remove CRLF and add execute permission

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 const firstArg = parseInt(process.argv[2]);
 const secondArg = parseInt(process.argv[3]);
-const result = any;
 
-function add(a, b) {
-    if (isNaN(firstArg, secondArg )) {
-        return NaN;
-    } else {
-    console.log( a + b)
-    }
-
+function add (a, b) {
+  return a + b;
 }
 
-console.log(add(firstArg, secondArg))
+console.log(add(firstArg, secondArg));
